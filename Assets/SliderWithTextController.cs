ffd8b7998dd79f95d62a9eca6f29a134a37495ba
alayham <alayham@hotmail.com>
2017-03-17 08:41:39 +0100

    Created a script to update the label when the slider value changes

---
+++ Assets/SliderWithTextController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderWithTextController : MonoBehaviour {
+
+    Text label;
+    Slider slider;
+    string initialLabel = "";
+
+	// Use this for initialization
+	void Start () {
+        label = transform.Find("Text").gameObject.GetComponent<Text>();
+        slider = transform.Find("Slider").gameObject.GetComponent<Slider>();
+        initialLabel = label.text;
+	}
+	
+    public void ChangeText()
+    {
+        label.text = initialLabel + " " + slider.value.ToString();
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
